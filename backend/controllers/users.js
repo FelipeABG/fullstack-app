2cf10236dbb810fac7a9ecf7eede9a27ecfc7adc
felipe <felipe.baleche2@gmail.com>
2025-03-21 09:15:21 -0300

    added new prettier configuration

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,10 +1,10 @@
 import db from "../db.js";
 
 export function get_users(_, res) {
-  const query = "select * from users;";
+   const query = "select * from users;";
 
-  db.query(query, (err, data) => {
-    if (err) return res.json(err);
-    return res.status(200).json(data);
-  });
+   db.query(query, (err, data) => {
+      if (err) return res.json(err);
+      return res.status(200).json(data);
+   });
 }
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,19 @@
 import { useEffect, useState } from "react";
 
 export default function App() {
-  let [data, set_data] = useState([]);
+   let [data, set_data] = useState([]);
 
-  useEffect(() => {
-    fetch("http://localhost:8000/users")
-      .then((response) => response.json())
-      .then((data) => set_data(data));
-  }, []);
+   useEffect(() => {
+      fetch("http://localhost:8000/users")
+         .then((response) => response.json())
+         .then((data) => set_data(data));
+   }, []);
 
-  return (
-    <div>
-      {data.map((user) => (
-        <p>{user.name}</p>
-      ))}
-    </div>
-  );
+   return (
+      <div>
+         {data.map((user) => (
+            <p>{user.name}</p>
+         ))}
+      </div>
+   );
 }
