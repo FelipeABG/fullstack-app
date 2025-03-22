88dcb2b91b24bcc00d0f18bdb194bd9931c6080a
Felipe Augusto <felipe.baleche2@gmail.com>
2025-03-21 21:49:24 -0300

    change to mysql2 package

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,10 @@
-import mysql from "mysql";
+import mysql from "mysql2";
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "felipe",
-  password: "root",
-  database: "web",
+   host: "localhost",
+   user: "felipe",
+   password: "root",
+   database: "web",
 });
 
 export default db;
